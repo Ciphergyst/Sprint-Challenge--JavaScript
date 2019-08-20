d7928deebfa3b0206e9fb40e4206ef582dc45f76
Ciphergyst <Jeremy McGrath>
2019-08-19 18:23:19 -0700

    refactored into class syntax

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,41 +1,26 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
 class CuboidMaker {
-  constructor(atrributes) {
-    this.length = atrributes.length;
-    this.width = atrributes.width;
-    this.height = atrributes.height;
-  }
-
-}
-console.log(CuboidMaker);
-
-
-class cuboid1 extends CuboidMaker {
-  constructor(volume) {
-    super(volume);
-  }
-   volume(){
-     return this.length * this.width * this.height;
-   }
-
-}
-
-
-class cuboid2 extends CuboidMaker {
-  constructor(surfaceArea) {
-  super(surfaceArea);
-}
-    surfaceArea(){
-      return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    constructor(length, width, height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 
+    volume() {
+        return this.length * this.width * this.height;
+    }
 
+    surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
 }
+
+
 // length: 4, width: 5, and height: 5 to cuboid.
-let newCub = new Cuboid(4, 5, 5);
 
 
+const newCuboid = new CuboidMaker(4, 5, 5);
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
